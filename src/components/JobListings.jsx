f37b31881dab6d81e67c70f11745ea5463ad3e32
baagdaas <saad.cse.kuet@gmail.com>
2025-12-20 15:59:57 +0600

    fetching data from API

---
+++ src/components/JobListings.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+import JobListing from './JobListing'
+
+const JobListings = ({isHome = false}) => {
+    // const jobListings = isHome? jobs.slice(0, 3): jobs;
+    const [jobs, setJobs] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+      const fetchJobs = async () => {
+        
+        try{
+          const res = await fetch('http://localhost:8000/jobs');
+          const data = await res.json();
+          setJobs(data);
+        }
+        catch (error) {
+          console.error('Error fetching jobs:', error);
+        }
+        finally {
+          setLoading(false);
+        }
+
+      }
+      fetchJobs();
+
+    } , []);
+
+    return (
+    <section className="bg-blue-50 px-4 py-10">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+          {isHome ? 'Recent Job Listings' : 'Browse Jobs'}
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          { loading? (<h2>Loading...</h2>) :(
+            <>
+              { jobs.map((job) => (
+                <JobListing key={job.id} job={job} />
+              )) }
+            </>
+            )
+          }
+            
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default JobListings
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from '../components/Hero.jsx'
+import HomeCards from '../components/HomeCards.jsx'
+import JobListings from '../components/JobListings.jsx'
+import ViewAllJobs from '../components/ViewAllJobs.jsx'
+
+const HomePage = () => {
+  return (
+    <>
+        <Hero />
+        <HomeCards />
+        <JobListings isHome={true} />
+        <ViewAllJobs />
+    </>
+  )
+}
+
+export default HomePage
