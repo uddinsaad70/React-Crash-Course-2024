9cc922137577856b84534d403f2fabdf83ad5887
baagdaas <saad.cse.kuet@gmail.com>
2025-12-28 10:26:45 +0530

    AddJobPage and DeleteJobPage are settled

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,40 @@ import MainLayout from "./layouts/MainLayout";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import JobPage, {JobLoader} from "./pages/JobPage";
+import AddJobPage from "./pages/AddJobPage";
+
+const App = () => {
+//add new job
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newJob)
+    });
+    return ; 
+};
+
+//delete job
+const deleteJob = async (id) => { 
+  const res = await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE',
+    });
+    return ;
+};
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path = '/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
-      <Route path="/jobs/:id" element={<JobPage />} loader={JobLoader} />
+      <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob}/>} />
+      <Route path="/jobs/:id" element={<JobPage deleteJob ={ deleteJob} />} loader={JobLoader} />
       <Route path="*" element={<NotFoundPage />} />
     </Route>
   )
 );
-
-const App = () => {
   return <RouterProvider router={router} />;
 };
 export default App
--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
-import { useParams, useLoaderData } from 'react-router-dom';
+import { useParams, useLoaderData, useNavigate } from 'react-router-dom';
 import { FaArrowLeft, FaMap, FaMapMarker } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
-const JobPage = () => {
-
+const JobPage = ({deleteJob}) => {
+    const navigate = useNavigate();
     const { id } = useParams();
     const job = useLoaderData();
+    const onDeleteClick = (jobId) => {
+      const confirm = window.confirm('Are you sure you want to delete this job?');
+      if(!confirm) {
+        return ;
+      }
+      deleteJob(jobId); 
+      toast.success('Job Deleted Successfully');
+      navigate('/jobs');
+    }
+
     return (
     <>
     <section>
@@ -87,7 +98,7 @@ const JobPage = () => {
                 className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >Edit Job</Link
               >
-              <button
+              <button onClick = { () => onDeleteClick(job.id)}
                 className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
               >
                 Delete Job
